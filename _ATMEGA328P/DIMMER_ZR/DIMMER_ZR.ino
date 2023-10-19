a3441145b9a8c641ce3501f4d276478fb66819f7
Do Van Tuan <108563552+Tuan285@users.noreply.github.com>
2023-10-19 14:59:38 +0700

    commit

---
+++ _ATMEGA328P/DIMMER_ZR/DIMMER_ZR.ino
@@ -0,0 +1,23 @@
+int LAMP = 6;
+int dim_val = 0;
+void setup() {
+  Serial.begin(9600);
+  pinMode(LAMP, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(2), zero_cross, CHANGE);
+}
+void zero_cross() {
+  // int dimming_time = (20 * dim_val);
+  int dimming_time = dim_val;
+  delayMicroseconds(dimming_time);
+  digitalWrite(LAMP, HIGH);
+  delayMicroseconds(100);
+  digitalWrite(LAMP, LOW);
+}
+void loop() {
+  int data = analogRead(A0);
+  // int data1 = map(data, 0, 1023, 850, 1100);
+  int data1 = map(data, 0, 1023, 0, 4500);
+  // Serial.println(data1);
+  dim_val = data1;
+  // delay(100);
+}
